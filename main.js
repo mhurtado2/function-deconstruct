b6fc6a9b6cd2bc82ae2d449ae2cf79431ce9a486
Shane <mhurtado1@berklee.edu>
2022-12-12 09:35:16 -0600

    func-deconstruct

---
+++ main.js
@@ -0,0 +1,91 @@
+/*
+My Hobbies / Bad paying job is being a musician,
+Here is how I practice and prepare for show
+*/
+
+
+const playLinesWithMetronome = () => {
+    // Code to warm up with a metronome playing single lines
+    //console.log task
+    console.log('practicing with metronome')
+}
+
+const playWithFamousRecordings = () => {
+    // Code to warm up my rhythm playing so i can find the pocket easier when playing live
+    //console.log task
+    console.log('practicing rhythm playing with your favorite tunes')
+}
+
+const playThroughShowTunes = () => {
+    // Code to play through the songs I will be playing later
+    //console.log task
+    console.log('play through tunes that will be played at show later')
+}
+
+const identifyMistakes = () => {
+    // Code to identify any areas where I made a mistake
+    //console.log task
+    console.log('identify mistakes you made in each tune')
+}
+
+const correctMistakes = () => {
+    // Code to correct mistake I made in my last playthrough of each song
+    //console.log task
+    console.log('correct mistakes you made last in the last playthrough ')
+}
+
+const packUp = () => {
+    // Code to pack up all the gear I will need for my show
+    //console.log task
+    console.log('pack up your gear')
+}
+
+const checkHaveAllGear = () => {
+    // Code to check if I have all of the gear I need to play later
+    //console.log task
+    console.log('make surew you have all necessary gear before leaving')
+}
+
+const driveToVenue = () => {
+    // Code to drive to venue
+    //console.log task
+    console.log('drive to venue')
+}
+
+const loadIn = () => {
+    // Code to load in gear
+    //console.log task
+    console.log('load all gear into venue')
+}
+
+const soundCheck = () => {
+    // Code to sound check
+    //console.log task
+    console.log('sound check with the band')
+}
+
+const waitToPlay = () => {
+    // Code to for me to wait till I play
+    //console.log task
+    console.log('wait till it is your time to rock')
+}
+
+const playShow = () => {
+    // Code to play show
+    //console.log task
+    console.log('play the show and rock out')
+}
+
+
+playLinesWithMetronome()
+playWithFamousRecordings()
+playThroughShowTunes()
+identifyMistakes()
+correctMistakes()
+packUp()
+checkHaveAllGear()
+driveToVenue()
+loadIn()
+soundCheck()
+waitToPlay()
+playShow()
